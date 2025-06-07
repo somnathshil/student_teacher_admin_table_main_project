3168fe0ceb9284d7ae9e25d93b5b5ef49cc28d32
somnath shil <somnathshil23@gmail.com>
2025-06-07 09:25:00 +0530

    otp input validation update

--- otp_input.php
+++ otp_input.php
@@ -29,6 +29,22 @@
 			background-color: #36e3b7;
 		}
 
+		.errMsg{
+			background-color: red;
+			color:white;
+		}
+
+		.errInput {
+  border: 1px solid red !important;
+  outline: none;           /* remove focus outline */
+  box-shadow: none !important; /* remove any shadows */
+}
+.sucrssInput {
+  border: 1px solid green !important;
+  outline: none;           /* remove focus outline */
+  box-shadow: none !important; /* remove any shadows */
+}
+
 	</style>
 </head>
 <body>
@@ -36,12 +52,104 @@
     <div class="formcont">
     	<form method="post" action="check_otp.php">
     		<label>Enter 4 digit OTP :</label><br>
-    		<input type="number" name="enteredOTP" class="input"><br>
+    		<input type="number" name="enteredOTP" class="input otpInput" oninput="otpValid()"><br>
+    		<div class="otpErrDiv" style="width:90%"></div><br>
 
     		<label>Create new Password :</label><br>
-    		<input type="text" name="enteredPass" class="input"><br>
+    		<input type="text" name="enteredPass" class="input passwordInput" oninput="passwordValid()"><span class="showPass"><i class="fa-solid fa-eye"></i></span><br>
+    		<div class="passwordErrDiv" style="width:90%"></div><br>
     		<button name="btn" class="formbtn">submit</button>
     	</form>
     </div>
+
+
+    <script type="text/javascript">
+
+      function otpValid(){
+		const otp = document.querySelector(".otpInput");
+		const errDiv = document.querySelector(".otpErrDiv");
+				if(otp.value === "" || otp.value === null){
+		             errDiv.innerHTML = "<p>You must enter otp</p>";
+		             otp.classList.remove("sucrssInput");
+		             errDiv.classList.add("errMsg");
+		             otp.classList.add("errInput");
+
+				}else{
+		              
+		              const otpRegex = /^[0-9]{4,}$/;
+		              if(otpRegex.test(otp.value)){
+		                    errDiv.innerHTML = "";
+		                    errDiv.classList.remove("errMsg");
+		                 otp.classList.remove("errInput");
+		                 otp.classList.add("sucrssInput");
+		                 
+		              }else{
+		              	 errDiv.innerHTML = "<p>Invalid Data</p>";
+		              	 otp.classList.remove("sucrssInput");
+		                errDiv.classList.add("errMsg");
+		                otp.classList.add("errInput");
+		              }
+				}
+		checkAllValid();
+	}
+	otpValid();
+
+
+
+    	function passwordValid(){
+		const password = document.querySelector(".passwordInput");
+		const errDiv = document.querySelector(".passwordErrDiv");
+				if(password.value === "" || password.value === null){
+		             errDiv.innerHTML = "<p>You must enter password</p>";
+		             password.classList.remove("sucrssInput");
+		             errDiv.classList.add("errMsg");
+		             password.classList.add("errInput");
+
+				}else{
+		              
+		              const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[#@$!%*?&])[A-Za-z\d@#$!%*?&]{8,}$/;
+		              if(passwordRegex.test(password.value)){
+		                    errDiv.innerHTML = "";
+		                    errDiv.classList.remove("errMsg");
+		                 password.classList.remove("errInput");
+		                 password.classList.add("sucrssInput");
+		                 
+		              }else{
+		              	 errDiv.innerHTML = "<p>Invalid Data</p>";
+		              	 password.classList.remove("sucrssInput");
+		                errDiv.classList.add("errMsg");
+		                password.classList.add("errInput");
+		              }
+				}
+		checkAllValid();
+	}
+	passwordValid();
+
+
+let isPassShow = false;
+const showPass = document.querySelector(".showPass");
+
+showPass.addEventListener("click",function(){
+    isPassShow = !isPassShow;
+   if(isPassShow){
+    document.querySelector(".showPass").innerHTML = "<i class='fa-solid fa-eye-slash'></i>";
+    document.querySelector(".passwordInput").type = "text";
+   }else{
+   	document.querySelector(".showPass").innerHTML = "<i class='fa-solid fa-eye'></i>";
+   	document.querySelector(".passwordInput").type = "password";
+   }
+});
+
+
+function checkAllValid() {
+  const otpValid = document.querySelector(".otpInput").classList.contains("sucrssInput");
+  const passwordValid = document.querySelector(".passwordInput").classList.contains("sucrssInput");
+
+  const allValid = otpValid &&  passwordValid;
+
+  document.querySelector(".formbtn").disabled = !allValid;
+}
+
+    </script>
 </body>
 </html>
